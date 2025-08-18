9f3c8b4f1b708b63a61aeb554b720c3320e081c5
Liliane Alves <lasp1@discente.ifpe.edu.br>
2025-08-18 23:49:38 +0000

    build: created an app

---
+++ app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+from functions.plot_ts import plot
+
+st.title('Historico de cotacoes')
+st.write('Veja o historico das cotações das empresas.')
+
+ticker = st.sidebar.text_input('Escolha o ticker:', value = 'AAPL')
+
+fig = plot(ticker)
+st.plotly_char(fig)
